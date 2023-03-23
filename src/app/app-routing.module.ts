565cd0cd0bb42a88fe0d9695cae39e0e43b3bb4e
rosanamrcm <mrcm1975@gmail.com>
2023-03-23 16:36:22 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { SchedulingComponent } from './pages/scheduling/scheduling.component';
+import { LoadingComponent } from './shared/loading/loading.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path:'register', component: RegisterComponent},
+  { path:'scheduling', component: SchedulingComponent},
+  { path: 'loading' , component: LoadingComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoadingComponent } from './shared/loading/loading.component';
+import { SchedulingComponent } from './pages/scheduling/scheduling.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoadingComponent,
+    SchedulingComponent
   ],
   imports: [
     BrowserModule,
